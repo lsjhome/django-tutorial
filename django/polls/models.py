9fb1fb2bffa72ce00c8f585801cfa1e0ba6b1149
lsjhome <lsjhome007@gmail.com>
2018-02-02 14:18:53 +0900

    Pull, Question을 admin에 등록

--- django/polls/models.py
+++ django/polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,6 +11,13 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        """
+        최근 날자인지 비교함
+        :return:
+        """
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -16,4 +25,8 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text
+        return '{} | {}'.format(
+            self.question.question_text,
+            self.choice_text
+        )
+
